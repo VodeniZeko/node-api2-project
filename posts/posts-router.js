149dfb83208287e7645bba326ceb243b4f6b94c4
VodeniZeko <salexy.es@gmail.com>
2020-03-10 23:45:53 -0700

    delete and post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -11,7 +11,6 @@ router.get("/", async (req, res) => {
     let posts = await db.find();
     res.status(200).json(posts);
   } catch (err) {
-    console.log(err);
     res
       .status(500)
       .json({ error: "The posts information could not be retrieved." });
@@ -61,11 +60,83 @@ router.get("/:id/comments", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+  } else {
+    try {
+      let id = await db.insert(req.body);
+      let post = await db.findById(id);
+      res.status(200).json(post);
+    } catch {
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the post to the database"
+        });
+    }
+  }
+});
+
+router.post("/:id/comments", async (req, res) => {
+  if (!req.body.text) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+  } else {
+    try {
+      let posts = await db.findById(req.params.id);
+      if (posts.length === 0) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        let id = await db.insertComment({
+          ...req.body,
+          post_id: req.params.id
+        });
+        let comments = await db.findCommentById(id);
+        res.status(201).json(comments[0]);
+      }
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the comment to the database"
+        });
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let posts = await db.findById(req.params.id);
+    if (posts.length === 0) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      const post = posts[0];
+      await db.remove(req.params.id);
+      res.status(200).json(post);
+    }
+  } catch {
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
+
 router.put("/:id", async (req, res) => {
   if (!req.body.title || !req.body.contents) {
-    res.status(400).json({
-      errorMessage: "Please provide title and contents for the post."
-    });
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
   } else {
     try {
       await db.update(req.params.id, req.body);
